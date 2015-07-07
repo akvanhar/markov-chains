152b0de8f00ed7c3dd5023ae637134dc5270b614
Hackbright Student <info@hackbrightacademy.com>
2015-07-07 12:27:34 -0700

    Added values to the dictionary

--- markov.py
+++ markov.py
@@ -5,18 +5,32 @@ def make_chains(corpus):
     """Takes input text as string; returns dictionary of markov chains."""
     
     #clean  up the text
-    corpus_file = open(corpus).read()
+    corpus_file = open(corpus).read().rstrip()
     clean_corpus = corpus_file.replace("\n", " ")
-
     #create dictionary keys
     corpus_list = clean_corpus.split(" ")
     corpus_dict = {}
+    tuple_list = []
     for i in range(len(corpus_list)-1):
         key = (corpus_list[i], corpus_list[i+1])
-        corpus_dict[key] = None   
-        print key
+        tuple_list.append(key)
+        corpus_dict[key] = [] 
+        #print key
 
-    return {}
+    #print tuple_list
+
+    
+    for x in range(len(corpus_list) -1):
+        list_tuple = (corpus_list[x], corpus_list[x+1])
+        for tup in corpus_dict.keys():
+            if tup == list_tuple and x <= (len(corpus_list)-3):
+                corpus_dict[tup].append(corpus_list[x+2])
+            #corpus_dict[tup] = value_list
+            
+    for key, value in corpus_dict.items():
+        print key, value   
+
+    return corpus_dict
 
 
 def make_text(chains):
