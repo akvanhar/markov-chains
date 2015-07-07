c79e6aa793148da74f2af2a2e685b9a82c8d4634
Hackbright Student <info@hackbrightacademy.com>
2015-07-07 12:51:34 -0700

    a non-looping Markov chain

--- markov.py
+++ markov.py
@@ -7,38 +7,52 @@ def make_chains(corpus):
     #clean  up the text
     corpus_file = open(corpus).read().rstrip()
     clean_corpus = corpus_file.replace("\n", " ")
+    
     #create dictionary keys
     corpus_list = clean_corpus.split(" ")
     corpus_dict = {}
     tuple_list = []
+
     for i in range(len(corpus_list)-1):
         key = (corpus_list[i], corpus_list[i+1])
         tuple_list.append(key)
         corpus_dict[key] = [] 
-        #print key
-
-    #print tuple_list
-
     
+    #append values into dictionary keys
     for x in range(len(corpus_list) -1):
         list_tuple = (corpus_list[x], corpus_list[x+1])
         for tup in corpus_dict.keys():
             if tup == list_tuple and x <= (len(corpus_list)-3):
                 corpus_dict[tup].append(corpus_list[x+2])
-            #corpus_dict[tup] = value_list
-            
-    for key, value in corpus_dict.items():
-        print key, value   
+
+    #to print a test dictionary, uncomment the two lines bellow        
+    #for key, value in corpus_dict.items():
+    #   print key, value   
 
     return corpus_dict
 
 
 def make_text(chains):
     """Takes dictionary of markov chains; returns random text."""
+    output_text = ""
+    random_key = random.choice(chains.keys())
+    random_word = random.choice(chains[random_key])
+    key_string = "%s %s" %(random_key[0], random_key[1])
+    print key_string
+    output_text += "%s %s" %(key_string, random_word)
+    print output_text
 
-    return "Here's some random text."
+    # while chains[random_key] != []:
+    #     #string_key = random_key[0] + " " + random_key[1] + random_word
+
+    #     random_key = random.choice(chains.keys())
+    #     random_word = random.choice(chains[random_key])
+    # print random_key, random_word
+    # return "Here's some random text."
 
 
+magic_dict_super_de_duper = make_chains("green-eggs.txt")
+make_text(magic_dict_super_de_duper)
 # Change this to read input_text from a file, deciding which file should
 # be used by examining the `sys.argv` arguments (if neccessary, see the
 # Python docs for sys.argv)
@@ -53,5 +67,3 @@ input_text = "Some text"
 
 #print random_text
 
-
-make_chains("green-eggs.txt")
